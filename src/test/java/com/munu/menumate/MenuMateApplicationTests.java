7b547c32be34311ef33a4f5629dc0839f417b362
2yuna <y.duck10002@gmail.com>
2023-04-11 18:04:49 +0900

    first commit

---
+++ src/test/java/com/munu/menumate/MenuMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munu.menumate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
